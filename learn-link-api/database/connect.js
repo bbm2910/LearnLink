468f34f76b98366ec9cb9f4efccab3d08f052ddf
Khavan <krajput123@hotmail.com>
2025-05-13 13:01:47 +0100

    Update sql and seed database

--- learn-link-api/database/connect.js
+++ learn-link-api/database/connect.js
@@ -1,11 +1,16 @@
+console.log('DB_URL:', process.env.DB_URL);
 const { Pool } = require('pg');
 
 const db = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    
+    connectionString: process.env.DB_URL,
+  
+
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT
 });
 
 module.exports = db;
--- learn-link-api/database/setup.js
+++ learn-link-api/database/setup.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const fs = require('fs');
 const db = require('./connect');
 
-const sql = fs.readFileSync('./learn-link-db/setup.sql').toString();
+const sql = fs.readFileSync('../learn-link-db/setup.sql').toString();
 
 db.query(sql)
   .then(data => {
