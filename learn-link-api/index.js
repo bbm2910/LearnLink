a6d87d95d341aa8d17465a030af0ccfa43169664
bobby2910 <b.mastacan@gmail.com>
2025-05-13 12:54:42 +0100

    Add calendar, add modal, modify right modal

--- learn-link-api/index.js
+++ learn-link-api/index.js
@@ -1,6 +1,6 @@
 const { app } = require("./app");
-// const port = process.env.PORT;
-const port = 3000; // Temporary change for testing
+const port = process.env.PORT;
+// const port = 3000;
 
 app.listen(port, () => {
   console.log(`API running on port: ${port}`);
