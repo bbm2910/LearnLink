9ffaf20d3b582df22aeddc5aa6ed9ec7fead05b8
Emma Acquah <emmaacquah@gmail.com>
2025-05-13 13:47:20 +0100

    Adjust index.js to use .env

--- learn-link-api/index.js
+++ learn-link-api/index.js
@@ -1,8 +1,7 @@
-require("dotenv").config()
-
+require("dotenv").config();
 const { app } = require("./app");
+
 const port = process.env.PORT;
-// const port = 3000;
 
 app.listen(port, () => {
   console.log(`API running on port: ${port}`);
