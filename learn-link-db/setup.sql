5393911f9c3b9980da478fab5f7132e04924c51c
MrDanielHo <daniel.kun.tin.ho@gmail.com>
2025-05-15 11:10:07 +0100

    simplified skills and skills description

--- learn-link-db/setup.sql
+++ learn-link-db/setup.sql
@@ -129,24 +129,23 @@ INSERT INTO dim_user (first_name, last_name, email, password, city, postcode, im
 
 -- Insert into skills
 INSERT INTO dim_skill (skill_name, skill_desc) VALUES
-('Music', 'Guitar Playing - Ability to perform rhythm and lead guitar parts on acoustic or electric guitar.'),
-('Music', 'Piano Proficiency - Skilled in playing classical and contemporary pieces on the piano.'),
-('Music', 'Drumming Technique - Expertise in percussion and drumming using a standard drum kit.'),
-('Music', 'Violin Performance - Trained in playing solo and ensemble pieces with the violin.'),
-('Music', 'Saxophone Improvisation - Capable of performing jazz and blues improvisations on the saxophone.'),
+('Guitar Playing', 'Ability to perform rhythm and lead guitar parts on acoustic or electric guitar.'),
+('Piano Proficiency', 'Skilled in playing classical and contemporary pieces on the piano.'),
+('Drumming Technique', 'Expertise in percussion and drumming using a standard drum kit.'),
+('Violin Performance', 'Trained in playing solo and ensemble pieces with the violin.'),
+('Saxophone Improvisation', 'Capable of performing jazz and blues improvisations on the saxophone.'),
 
-('Programming', 'Proficient JavaScript - building web applications.'),
-('Programming', 'Experienced Python - data analysis and scripting.'),
-('Programming', 'Java - developing enterprise-level software .'),
-('Programming', 'Capable C++ - building efficient system-level code.'),
-('Programming', 'Knowledgeable Haskell - functional programming.'),
+('Proficient JavaScript', 'Building web applications.'),
+('Experienced Python', 'Data analysis and scripting.'),
+('Java', 'Developing enterprise-level software.'),
+('Capable C++', 'Building efficient system-level code.'),
+('Knowledgeable Haskell', 'Functional programming.'),
 
-('Cooking', 'Baking Techniques - Skilled in preparing breads, pastries, and cakes using precise baking methods.'),
-('Cooking', 'Knife Skills - Proficient in professional knife handling, including slicing, dicing, and julienning.'),
-('Cooking', 'Sauce Preparation - Experienced in making classic sauces such as béchamel, hollandaise, and demi-glace.'),
-('Cooking', 'Grilling Mastery - Capable of grilling meats, vegetables, and seafood to optimal doneness and flavor.'),
-('Cooking', 'International Cuisine - Knowledgeable in preparing dishes from various global cuisines including Thai, Italian, and Indian.');
-
+('Baking Techniques', 'Skilled in preparing breads, pastries, and cakes using precise baking methods.'),
+('Knife Skills', 'Proficient in professional knife handling, including slicing, dicing, and julienning.'),
+('Sauce Preparation', 'Experienced in making classic sauces such as béchamel, hollandaise, and demi-glace.'),
+('Grilling Mastery', 'Capable of grilling meats, vegetables, and seafood to optimal doneness and flavor.'),
+('International Cuisine', 'Knowledgeable in preparing dishes from various global cuisines including Thai, Italian, and Indian.');
 
 -- Insert into time
 INSERT INTO dim_time (action_date, year, month, day, hour, minute, second) VALUES
