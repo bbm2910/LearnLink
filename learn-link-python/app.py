ed43c575318de14120e32b5b906587fe00be81a7
Emma Acquah <emmaacquah@gmail.com>
2025-05-15 15:04:53 +0100

    Add user skills chart endpoint

--- learn-link-python/app.py
+++ learn-link-python/app.py
@@ -13,6 +13,37 @@ app = Flask(__name__)
 def index():
     return "Welcome to LearnLink's Python data visualisation API!"
 
+@app.post("/current-skills-chart")
+def generate_current_skills_visualisation():
+    data = request.json
+
+    skills = data.get('skills', []) # array of skill names
+    sessions = data.get('sessions', []) # array of int value of no. of sessions
+
+    # Data validation
+    if not skills or not sessions or len(skills) != len(sessions):
+        return jsonify({'error': 'Invalid data received'}), 400
+
+    # Ubong Workings
+    plt.figure(figsize=(8, 4))
+    sns.set_style("white")
+    sns.barplot(x=sessions, y=skills, palette='Blues_d')
+    plt.title("Skills You're Currently Learning")
+    plt.xlabel("Number of Sessions")
+    plt.ylabel("Skill")
+    plt.tight_layout()
+    plt.show()
+
+    # Generate image & HTML
+    img_bytes = io.BytesIO()
+    plt.savefig(img_bytes, format='png')
+    img_bytes.seek(0)
+    img_base64 = base64.b64encode(img_bytes.getvalue()).decode('utf-8')
+    bar_chart_html = f'<img src="data:image/png;base64,{img_base64}" />'
+
+    return jsonify({'visualisation_html': bar_chart_html})
+
+
 @app.post("/top-skills-chart")
 def generate_top_skills_visualisation():
     data = request.json
