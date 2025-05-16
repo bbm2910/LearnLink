ac098ec94343052af27eef6b03e815e996fe850d
Emma Acquah <emmaacquah@gmail.com>
2025-05-16 18:00:07 +0100

    Add skills_cat property to Skill model class

--- learn-link-api/models/Skill.js
+++ learn-link-api/models/Skill.js
@@ -1,28 +1,29 @@
 const db = require("../database/connect");
 class Skill {
-  constructor({ skill_id, skill_name, skill_desc }) {
+  constructor({ skill_id, skill_cat, skill_name, skill_desc }) {
     this.skill_id = skill_id;
+    this.skill_cat = skill_cat;
     this.skill_name = skill_name;
     this.skill_desc = skill_desc;
   }
 
   static async create(data) {
-    const { skill_name, skill_desc } = data;
+    const { skill_cat, skill_name, skill_desc } = data;
     let response = await db.query(
-      "INSERT INTO dim_skill (skill_name, skill_desc) VALUES ($1, $2) returning skill_id",
-      [skill_name, skill_desc]
+      "INSERT INTO dim_skill (skill_cat, skill_name, skill_desc) VALUES ($1, $2, $3) returning skill_id",
+      [skill_cat, skill_name, skill_desc]
     );
     const newId = response.rows[0].skill_id;
     const newSkill = await Skill.getOneById(newId);
     return newSkill;
   }
 
-  static getOneById = async (skill_id) => {
+  static async getOneById(skill_id) {
     const response = await db.query(
-      "SELECT * FROM dim_user WHERE skill_id = $1;",
+      "SELECT * FROM dim_skill WHERE skill_id = $1;",
       [skill_id]
     );
-    if (response.rows.length != 1) {
+    if (response.rows.length !== 1) {
       throw Error("Unable to locate skill");
     }
     return new Skill(response.rows[0]);
