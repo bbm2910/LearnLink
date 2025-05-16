5050e8e0d792ab61b593a16b344c52413f7701ff
Emma Acquah <emmaacquah@gmail.com>
2025-05-16 12:36:23 +0100

    Add dashboard-skils.js script & update HTML page.

--- learn-link-api/helpers/dataProcessor.js
+++ learn-link-api/helpers/dataProcessor.js
@@ -1,10 +1,6 @@
 const formatTopSkillsData = (dbQueryResult) => {
   const skillNames = dbQueryResult.map( (o) => o.skill_name);
-<<<<<<< HEAD
   const learnerCounts = dbQueryResult.map( (o) => parseInt(o.number_of_learners));
-=======
-  const learnerCounts = dbQueryResult.map( (o) => o.number_of_learners);
->>>>>>> ec79b29 (Data helper functions)
 
   return {
     skills: skillNames,
@@ -14,11 +10,7 @@ const formatTopSkillsData = (dbQueryResult) => {
 
 const formatCurrentSkillsData = (dbQueryResult) => {
   const skillNames = dbQueryResult.map( (o) => o.skill_name);
-<<<<<<< HEAD
   const learningSessionCounts = dbQueryResult.map( (o) => parseInt(o.number_of_sessions));
-=======
-  const learningSessionCounts = dbQueryResult.map( (o) => o.number_of_sessions);
->>>>>>> ec79b29 (Data helper functions)
 
   return {
     skills: skillNames,
