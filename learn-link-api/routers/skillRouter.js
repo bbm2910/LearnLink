ce7c137c6e7b45ab055f4a7e24346d9c6d7bab4c
bobby2910 <b.mastacan@gmail.com>
2025-05-16 12:45:22 +0100

    add skills on user page from the db

--- learn-link-api/routers/skillRouter.js
+++ learn-link-api/routers/skillRouter.js
@@ -4,10 +4,12 @@ const skillController = require("../controllers/skillController");
 
 const skillRouter = Router();
 
-router.post("/", skillController.createSkill);
-router.get("/:id", skillController.getSkillById);
-router.get("/", skillController.searchSkills);
+skillRouter.get("/user/:userId/skills", skillController.getUserSkills);
+
+skillRouter.post("/", skillController.createSkill);
+skillRouter.get("/:id", skillController.getSkillById);
+skillRouter.get("/", skillController.searchSkills);
 
 module.exports = {
-    skillRouter
-}
+  skillRouter,
+};
