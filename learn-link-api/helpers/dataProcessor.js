9481fc745fb521cf84da0f8ddde1265a72b62b42
Emma Acquah <emmaacquah@gmail.com>
2025-05-16 08:54:32 +0100

    Data helper functions

--- learn-link-api/helpers/dataProcessor.js
+++ learn-link-api/helpers/dataProcessor.js
@@ -1,6 +1,10 @@
 const formatTopSkillsData = (dbQueryResult) => {
   const skillNames = dbQueryResult.map( (o) => o.skill_name);
+<<<<<<< HEAD
   const learnerCounts = dbQueryResult.map( (o) => parseInt(o.number_of_learners));
+=======
+  const learnerCounts = dbQueryResult.map( (o) => o.number_of_learners);
+>>>>>>> ec79b29 (Data helper functions)
 
   return {
     skills: skillNames,
@@ -10,7 +14,11 @@ const formatTopSkillsData = (dbQueryResult) => {
 
 const formatCurrentSkillsData = (dbQueryResult) => {
   const skillNames = dbQueryResult.map( (o) => o.skill_name);
+<<<<<<< HEAD
   const learningSessionCounts = dbQueryResult.map( (o) => parseInt(o.number_of_sessions));
+=======
+  const learningSessionCounts = dbQueryResult.map( (o) => o.number_of_sessions);
+>>>>>>> ec79b29 (Data helper functions)
 
   return {
     skills: skillNames,
